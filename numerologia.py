52e3d0f9cc1a053bdd4e03e10f7161281f0d4981
Ana Claudia Bazzo Ghiraldi <anabazzo@gmail.com>
2020-07-22 21:40:53 -0300

    Implementado cálculo de numerologia com base no nome

--- numerologia.py
+++ numerologia.py
@@ -1,3 +1,48 @@
-def calcula_nome():
-    return True
-    
+from re import sub
+
+def remove_caract_espec(nome):
+  novo_nome = sub(r'[ãáàâä]', 'a', nome)
+  novo_nome = sub(r'[õóòôö]', 'o', novo_nome)
+  return novo_nome
+
+def calcula_nome(nome):
+  alfabeto = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 1,
+    'k': 2,
+    'l': 3,
+    'm': 4,
+    'n': 5,
+    'o': 6,
+    'p': 7,
+    'q': 8,
+    'r': 9,
+    's': 1,
+    't': 2,
+    'u': 3,
+    'v': 4,
+    'w': 5,
+    'x': 6,
+    'y': 7,
+    'z': 8
+  }
+
+  nome_valido = remove_caract_espec(nome)
+
+  nome_completo = list(nome_valido)
+
+  soma = 0
+
+  for letra in nome_completo: 
+    soma = soma + alfabeto.get(letra, 0)
+
+
+  return soma
--- teste.py
+++ teste.py
@@ -3,8 +3,20 @@ import unittest
 
 class TesteNumerologia(unittest.TestCase):
 
-  def test_calculo(self):
-    self.assertEqual(calcula_nome(), True)
+  def test_calculo_a(self):
+    self.assertEqual(calcula_nome('a'), 1)
+
+  def test_calculo_b(self):
+    self.assertEqual(calcula_nome('b'), 2)
+
+  def test_calculo_ana(self):
+    self.assertEqual(calcula_nome('ana'), 7)
+
+  def test_calculo_anaclaudia(self):
+    self.assertEqual(calcula_nome('ana claudia'), 31)
+
+  def test_calculo_joao(self):
+    self.assertEqual(calcula_nome('jõãõ'), 14)
 
 if __name__ == '__main__':
     unittest.main()
